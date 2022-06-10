6ee0095613b72d554922365523a3c7991cf64b81
Hamza Sohail SCM <hsoahil.mscs21seecs@seecs.edu.pk>
2022-06-11 03:19:10 +0500

    delete endpoint

--- src/images/images.controllers.js
+++ src/images/images.controllers.js
@@ -1,25 +1,44 @@
 import { Images } from './images.model.js';
 
 export const getMany = async (req, res) => {
-  try {
-    const createdBy = req.query.createdBy;
-    console.log({ createdBy });
-    const doc = await Images.find({ createdBy });
-    res.status(200).json({ data: doc });
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    res.status(400).end();
-  }
+	try {
+		const createdBy = req.query.createdBy;
+		const doc = await Images.find({ createdBy });
+		res.status(200).json({ data: doc });
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error(e);
+		res.status(400).end();
+	}
 };
 
 export const createOne = async (req, res) => {
-  try {
-    const doc = await Images.create({ ...req.body });
-    res.status(201).json({ data: doc });
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    res.status(400).end();
-  }
+	try {
+		const doc = await Images.create({ ...req.body });
+		res.status(201).json({ data: doc });
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error(e);
+		res.status(400).end();
+	}
+};
+
+export const removeOne = async (req, res) => {
+	try {
+		const removed = await Images.findOneAndRemove({
+			_id: req.params.id,
+		});
+
+		if (!removed) {
+			// eslint-disable-next-line no-console
+			console.error('removeOne - !removed');
+			return res.status(400).end();
+		}
+
+		return res.status(200).json({ data: removed });
+	} catch (e) {
+		// eslint-disable-next-line no-console
+		console.error(e);
+		res.status(400).end();
+	}
 };
--- src/images/images.router.js
+++ src/images/images.router.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { getMany, createOne } from './images.controllers.js';
+import { getMany, createOne, removeOne } from './images.controllers.js';
 
 const router = Router();
 
 // /api/users
 router.route('/').get(getMany).post(createOne);
+
+// /api/users /: id
+router.route('/:id').delete(removeOne);
 export default router;
